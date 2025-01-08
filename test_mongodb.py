5d812d3fdaaf73053c506bfc5952387198473eb7
Jai Vadula <jaivadula@gmail.com>
2025-01-08 18:22:27 +0530

    Data ingestion component completed

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,10 +1,9 @@
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://openjai456:<password>@cluster0.cvuij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://openjai456:Admin456@cluster0.cvuij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri)
 
 # Send a ping to confirm a successful connection
 try:
